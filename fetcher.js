f68dcd1619ee84858ee728fb8458be96f467b667
okzapradhana <okzamahendra29@gmail.com>
2021-04-21 16:05:18 +0800

    feat(geojson): create new simplified geojson with 0.05 tolerance

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
+const simplify = require('simplify-geojson');
 const BASE_PATH = './Indonesia-Postal-And-Area/data/geojson/62';
 const OUT_PATH = './indonesia-cities.json';
+const OUT_SIMPLIFY_PATH = './simplified-indonesia-cities.json';
+const TOLERANCE = 0.05;
 
 function getDirectories(dirPath) {
   const paths =
@@ -26,7 +29,10 @@ function main() {
 
   const geojson = { "type": "FeatureCollection" };
   geojson.features = geofeatures;
+  const simplifiedGeojson = simplify(geojson, TOLERANCE);
+
   fs.writeFileSync(OUT_PATH, JSON.stringify(geojson));
+  fs.writeFileSync(OUT_SIMPLIFY_PATH, JSON.stringify(simplifiedGeojson));
 }
 
 main();
